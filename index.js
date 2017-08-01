6926f936706e5e2c081c7fc125dffabdd857a040
David F.E <david@Air-de-David.lan>
2017-08-01 11:54:42 +0200

    Refactorizando apph

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require('./services/passport');
+
 const app = express();
 
+require('./routes/authRoutes')(app);
 /*
 app.get('/', (req, res) => {
-  res.send({bye: 'David'});
+  res.send({Hola: 'David'});
 });
 */
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      secretID: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('acces Token', accessToken);
-      console.log('Refresh Token', refreshToken);
-      console.log('Profile:', profile);
-    }
-  )
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
