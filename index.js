47555e4c0ad02095f6eb71614755d331da539e09
David F.E <david@Air-de-David.lan>
2017-09-11 19:26:08 +0200

    Corrección en index.js

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ if (process.env.NODE_ENV === 'production') {
   // Express esta proporcionando  index.html
   // si este no reconoce la ruta
   const path = require('path');
-  app.get(*,(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
