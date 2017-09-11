7438a8bd7cbeeeac176b054b02074fac583d6a65
David F.E <david@Air-de-David.lan>
2017-09-11 19:17:12 +0200

    Añadido script en package.json heroku-postbuild

--- index.js
+++ index.js
@@ -24,5 +24,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express esta proporcionando los assets de producción
+  // como main.js o main.cookie-session
+  app.use(express.static('client/build'));
+  // Express esta proporcionando  index.html
+  // si este no reconoce la ruta
+  const path = require('path');
+  app.get(*,(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
